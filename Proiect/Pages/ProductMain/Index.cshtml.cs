18dc901f8c22d118a25e4d78c2b8f26bd9a3bee5
Dani <Dani@DESKTOP-6L6QVSA>
2024-01-20 12:38:35 +0200

    Add project files.

---
+++ Proiect/Pages/ProductMain/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Products.Models;
+using Proiect.Data;
+
+namespace Proiect.Pages.ProductMain
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Proiect.Data.ProiectContext _context;
+
+        public IndexModel(Proiect.Data.ProiectContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Product.ToListAsync();
+        }
+    }
+}
